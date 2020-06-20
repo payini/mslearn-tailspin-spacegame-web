d32565977a97541850d3433b6f0144e14ac81b0d
Gerardo Melendrez <payini@hotmail.com>
2020-06-20 12:05:53 -0700

    Brought FavititeMap back.

--- Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
@@ -1,7 +1,8 @@
-﻿
-CREATE TABLE [dbo].[Profiles] (
-    [id]                INT           NOT NULL,
-    [userName]          NVARCHAR (50) NOT NULL,
-    [avatarUrl]         NVARCHAR (50) NULL,
+﻿CREATE TABLE [dbo].[Profiles]
+(
+    [id] INT NOT NULL,
+    [userName] NVARCHAR (50) NOT NULL,
+    [avatarUrl] NVARCHAR (50) NULL,
+    [favoriteMap] NVARCHAR(50) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
